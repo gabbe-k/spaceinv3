cd4ba9099a7e4241c7bec7c30d34bd27616b74cb
gabbe-k <44163424+gabbe-k@users.noreply.github.com>
2020-02-24 10:09:52 +0100

    Initial commit

---
+++ src/spaceinv/model/Gun.java
@@ -0,0 +1,16 @@
+package spaceinv.model;
+
+
+import static spaceinv.model.SI.*;
+
+/*
+ *    A Gun for the game
+ *    Can only fire one projectile at the time
+ */
+public class Gun  {
+
+
+
+
+
+}
